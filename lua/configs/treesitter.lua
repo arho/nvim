2678ff203c3baf4398e18f8c4548c997df685ed2
Ardavan Hosseini <arda7an@gmail.com>
2025-06-20 23:12:34 +0200

    Add mason and stuff

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,11 +1,17 @@
 local options = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-        "html", "css", "c", "bash",
-        "bicep", "cpp", "csv", "diff", "go", "goctl", "gomod", "gosum", "gotmpl", "gowork",
-        "hcl", "ini", "helm", "java", "jq", "jinja", "jinja_inline", "json",
-        "nginx", "python", "regex", "ruby", "rust", "scss", "sql", "ssh_config", "terraform", "toml",
-        "typescript", "xml", "yaml",
-  		},
+  ensure_installed = {
+  	"vim", "lua", "vimdoc",
+    "html", "css", "c", "bash",
+    "bicep", "cpp", "csv", "diff", "go", "goctl", "gomod", "gosum", "gotmpl", "gowork",
+    "hcl", "ini", "helm", "java", "jq", "jinja", "jinja_inline", "json",
+    "nginx", "python", "regex", "ruby", "rust", "scss", "sql", "ssh_config", "terraform", "toml",
+    "typescript", "xml", "yaml",
+  },
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
 }
-return options
+require("nvim-treesitter.configs").setup(options)
